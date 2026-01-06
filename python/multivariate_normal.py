2f5201958f5d8f643084f47f1b56145ba9ab818d
mathias <mathias.ollu@polytechnique.edu>
2026-01-06 09:37:28 +0000

    add curse of dim experiments

--- python/multivariate_normal.py
+++ python/multivariate_normal.py
@@ -28,10 +28,13 @@ if __name__ == '__main__':
     Gaussian_model = MVN(mu, A)
     Cauchy_model = Cauchy(mu_cauchy)
     
-    x0 = np.random.normal(0,1, [10,2])
+    x0 = np.random.normal(0,1, [100,1])
     theta_gauss = SVGD().update(x0, Gaussian_model.dlnprob, n_iter=1000, stepsize=0.01)
+    
     x0 = np.random.normal(0,1, [1000,1])
+
     theta_cauchy = SVGD().update(x0, Cauchy_model.dlnprob, n_iter=1000, stepsize=0.01)
+    cauchy_ground_truth= np.random.standard_cauchy(1000)
     
     print("Gaussian ground truth: ", mu)
     print(theta_gauss.shape)
@@ -41,10 +44,12 @@ if __name__ == '__main__':
     print(theta_cauchy.shape)
     print("svgd: ", np.mean(theta_cauchy))
 
-    plt.hist2d(theta_gauss[:,0], theta_gauss[:,1])
+    plt.hist(theta_gauss)
     plt.title('SVGD Gaussian Samples')
     plt.show()
     
-    plt.hist(theta_cauchy)
+    plt.hist(theta_cauchy, label='SVGD Cauchy Samples', density=True, alpha=0.5, color='blue')
+    plt.hist(cauchy_ground_truth, label='Ground Truth Cauchy Samples', alpha=0.5, color='orange', density=True)
     plt.title('SVGD Cauchy Samples')
     plt.show()
+    
